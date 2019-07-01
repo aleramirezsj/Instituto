0d2a0ea86e12c1a39952aa5ae56b0632d3fcd58a
aleramirezsj <aleramirezsj@gmail.com>
2019-07-01 01:17:11 -0300

    Agregado del Proyecto Modelos, del paquete Entity Framework (desde nuget) y activación de las autoMigrations

---
+++ Modelos/Modelos/BibliotecaContext.cs
@@ -0,0 +1,19 @@
+﻿using Modelos.Migrations;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Modelos
+{
+    public class BibliotecaContext : DbContext
+    {
+        public BibliotecaContext() : base("BibliotecaContext")
+        {
+            Database.SetInitializer<BibliotecaContext>(
+              new MigrateDatabaseToLatestVersion<BibliotecaContext, Configuration>());
+        }
+    }
+}
