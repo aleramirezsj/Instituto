bb1fbbf801a5a32c35170e2c872ae798960650e0
SrAxel <Thesense@outlook.es>
2024-11-04 17:51:22 -0300

    Cambios UI Materias respetando CiclosLectivos, Creacion IMateriaState y Cambios en MateriaView.Designer

---
+++ InstitutoDesktop/Interfaces/Commons/Materias/IMateriasState.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InstitutoDesktop.Interfaces.Commons.Materias
+{
+    public interface IMateriasState
+    {
+        Task ObtenerListasAsync();
+        void CargarCboCarreras();
+        void CargarCboAnioCarreras();
+        Task CargarDatosEnGrillaAsync();
+    }
+}
