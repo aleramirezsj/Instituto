7d17801a61b544f642883f016c92e1f2d6dada4c
Rubén Alejandro Ramirez <aleramirezsj@gmail.com>
2024-11-28 17:33:59 -0300

    Actualización de estilos y métodos de seguridad
    Se han realizado los siguientes cambios:
    
    - `TableMesaExamen.razor`: Envoltura de contenido de celdas en `<span>` con tamaño de fuente del 75% y uso de `<strong>` para el nombre de la materia.
    - `TableMesaExamenDocente.razor`: Modificación de formatos de encabezados y envoltura de contenido de celdas en `<span>` con tamaño de fuente del 75%.
    - `ObjectExtensions.cs`: Añadido método de extensión `GetHashSha256` para obtener el hash SHA256 de una cadena de texto.
    - `Horarios.razor`: Textos de botones modificados para mayor concisión.
    - `Registrarse.razor`: Encriptación de contraseña del usuario utilizando `GetHashSha256` antes de almacenarla en la caché.

--- InstitutoWeb/ExtensionMethod/ObjectExtensions.cs
+++ InstitutoWeb/ExtensionMethod/ObjectExtensions.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Reflection;
+using System.Security.Cryptography;
+using System.Text;
 
 namespace InstitutoWeb.ExtensionMethod
 {
@@ -7,6 +9,20 @@ namespace InstitutoWeb.ExtensionMethod
 
     public static class ObjectExtensions
     {
+        public static string GetHashSha256(this string textoAEncriptar)
+        {
+            // Create a SHA256   
+            using SHA256 sha256Hash = SHA256.Create();
+            // ComputeHash - returns byte array  
+            byte[] bytes = sha256Hash.ComputeHash(Encoding.UTF8.GetBytes(textoAEncriptar));
+            // Convert byte array to a string   
+            StringBuilder hashObtenido = new StringBuilder();
+            for (int i = 0; i < bytes.Length; i++)
+            {
+                hashObtenido.Append(bytes[i].ToString("x2"));
+            }
+            return hashObtenido.ToString();
+        }
         public static void CopyPropertiesTo<T>(this T source, T destination)
         {
             if (source == null || destination == null)
