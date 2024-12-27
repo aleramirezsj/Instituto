7a2f308b49e0f3b01b0f81793dc8a9a028c87b80
Rubén Alejandro Ramirez <aleramirezsj@gmail.com>
2024-12-27 06:56:26 -0300

    Añadir propiedad SubFilters y reestructurar código
    Se ha añadido una nueva propiedad `SubFilters` de tipo `List<FilterDTO>?` a la clase `FilterDTO` para manejar subfiltros en expresiones lógicas.
    Además, se ha reestructurado el código para mejorar la legibilidad, moviendo los comentarios a líneas separadas justo antes de las propiedades correspondientes.

--- InstitutoServices/Class/FilterDTO.cs
+++ InstitutoServices/Class/FilterDTO.cs
@@ -8,9 +8,14 @@ namespace InstitutoServices.Class
 {
     public class FilterDTO
     {
-        public string PropertyName { get; set; } = string.Empty; // Nombre de la propiedad a filtrar
-        public string Operation { get; set; } = string.Empty;   // Operación (por ejemplo: "Equals", "Contains", etc.)
-        public string? Value { get; set; }                      // Valor del filtro
+        public string PropertyName { get; set; }
+        // Nombre de la propiedad a filtrar
+        public string Operation { get; set; }
+        // Operación (por ejemplo: "Equals", "Contains", etc.)
+        public string? Value { get; set; }
+        // Valor de la propiedad a filtrar
+        public List<FilterDTO>? SubFilters { get; set; }
+        // Subfiltros para expresiones lógicas
     }
 
 }
