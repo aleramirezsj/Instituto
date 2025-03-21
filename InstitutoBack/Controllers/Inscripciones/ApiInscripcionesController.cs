e4e30346d4ed14c7f92286cc23276fc3c54789d6
Rubén Alejandro Ramirez <aleramirezsj@gmail.com>
2025-03-21 01:27:02 -0300

    Mejoras en lógica de inscripción y UI
    Se han añadido directivas `using` en `ApiInscripcionesController.cs` para nuevas dependencias. En `Getinscripciones`, `GetInscripcion`, `PutInscripcion`, `PostInscripcion` y `DeleteInscripcion` se han añadido inclusiones de entidades relacionadas y `AsNoTracking` para mejorar el rendimiento. En `PutInscripcion` y `PostInscripcion` se ha añadido lógica para manejar detalles de inscripciones.
    
    En `Inscripcion.cs`, se ha añadido una lista de `detallesInscripciones`. En `InstitutoWeb.csproj`, se ha añadido `InscripcionMateriasPDF.razor`. En `NavMenu.razor`, se ha añadido un enlace de navegación para "Inscripción a materias". En `EditarInscripcion.razor` y `NuevaInscripcion.razor`, se ha eliminado el código existente. En `Inscripciones.razor`, se ha reestructurado la lógica de inscripción a materias y generación de PDFs. En `InscripcionMateriasPDF.razor`, se ha añadido un componente para mostrar la inscripción en formato PDF.
    
    Se ha inyectado `SweetAlertService` en `ListCardInscripcionMaterias.razor`. Se ha añadido una estructura de pestañas (`nav-tabs`) para mostrar los años de carrera disponibles y lógica para seleccionar y mostrar materias. Se ha añadido un botón para finalizar la inscripción con una alerta de confirmación. Se han añadido métodos `HandleFinalizarInscripcion` y `ToggleSeleccion`, y se ha sobrescrito `OnInitialized` para inicializar listas. Se ha añadido un archivo CSS para estilizar tarjetas y pestañas de navegación, incluyendo una clase `.card-seleccionada` y efectos visuales.

--- InstitutoBack/Controllers/Inscripciones/ApiInscripcionesController.cs
+++ InstitutoBack/Controllers/Inscripciones/ApiInscripcionesController.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -9,6 +9,8 @@ using InstitutoServices.Models;
 using InstitutoBack.DataContext;
 using InstitutoServices.Models.Inscripciones;
 using InstitutoServices.Class;
+using InstitutoServices.Models.Commons;
+using InstitutoServices.Models.MesasExamenes;
 
 namespace InstitutoBack.Controllers.Inscripciones
 {
@@ -27,7 +29,14 @@ namespace InstitutoBack.Controllers.Inscripciones
         [HttpGet]
         public async Task<ActionResult<IEnumerable<Inscripcion>>> Getinscripciones()
         {
-            return await _context.inscripciones.Include(i => i.Carrera).Include(i => i.Alumno).ToListAsync();
+            return await _context.inscripciones
+                .Include(i => i.Carrera)
+                .Include(i => i.Alumno)
+                .Include(i=>i.PeriodoInscripcion)
+                .Include(i=>i.detallesInscripciones)
+                    .ThenInclude(d=>d.Materia)
+                    .ThenInclude(m=>m.AnioCarrera).AsNoTracking()
+                    .ToListAsync();
         }
 
         [HttpPost("filter")]
@@ -39,7 +48,13 @@ namespace InstitutoBack.Controllers.Inscripciones
             {
                 return BadRequest("Invalid filter expression.");
             }
-            return await _context.inscripciones.Include(i => i.Carrera).Include(i => i.Alumno).Where(filterExpression).ToListAsync();
+            return await _context.inscripciones
+                .Include(i => i.Carrera)
+                .Include(i => i.Alumno)
+                .Include(i=>i.PeriodoInscripcion)
+                .Include(i=>i.detallesInscripciones)
+                    .ThenInclude(d=>d.Materia)
+                    .ThenInclude(m=>m.AnioCarrera).Where(filterExpression).AsNoTracking().ToListAsync();
         }
 
 
@@ -47,7 +62,14 @@ namespace InstitutoBack.Controllers.Inscripciones
         [HttpGet("{id}")]
         public async Task<ActionResult<Inscripcion>> GetInscripcion(int id)
         {
-            var inscripcion = await _context.inscripciones.FindAsync(id);
+            var inscripcion = await _context.inscripciones
+                .Include(i => i.Carrera)
+                .Include(i => i.Alumno)
+                .Include(i => i.PeriodoInscripcion)
+                .Include(i => i.detallesInscripciones)
+                    .ThenInclude(d => d.Materia)
+                    .ThenInclude(m => m.AnioCarrera).AsNoTracking()
+                    .FirstOrDefaultAsync(i => i.Id.Equals(id));
 
             if (inscripcion == null)
             {
@@ -62,6 +84,58 @@ namespace InstitutoBack.Controllers.Inscripciones
         [HttpPut("{id}")]
         public async Task<IActionResult> PutInscripcion(int id, Inscripcion inscripcion)
         {
+            if (inscripcion.Carrera != null) _context.Attach(inscripcion.Carrera);
+            if (inscripcion.PeriodoInscripcion != null) _context.Attach(inscripcion.PeriodoInscripcion);
+            if (inscripcion.PeriodoInscripcion.CicloLectivo != null) _context.Attach(inscripcion.PeriodoInscripcion.CicloLectivo);
+
+            foreach (var insc_carrera in inscripcion.Alumno.InscripcionesACarreras)
+            {
+                var AttachCarreraExist = _context.Set<Carrera>().Local.FirstOrDefault(entry => entry.Id.Equals(insc_carrera.Carrera.Id));
+                if (AttachCarreraExist == null)
+                    _context.Attach(insc_carrera.Carrera);
+                else
+                    insc_carrera.Carrera = AttachCarreraExist;
+            }
+            if (inscripcion.Alumno != null) _context.Attach(inscripcion.Alumno);
+
+            foreach (var detalle in inscripcion.detallesInscripciones)
+            {
+                //detalle.Materia.AnioCarrera = null;
+                var AttachAnioExist = _context.Set<AnioCarrera>().Local.FirstOrDefault(entry => entry.Id.Equals(detalle.Materia.AnioCarrera.Id));
+                var AttachCarreraExist = _context.Set<Carrera>().Local.FirstOrDefault(entry => entry.Id.Equals(detalle.Materia.AnioCarrera.Carrera.Id));
+                if (AttachCarreraExist == null)
+                    _context.Attach(detalle.Materia.AnioCarrera.Carrera);
+                else
+                    detalle.Materia.AnioCarrera.Carrera = AttachCarreraExist;
+                if (AttachAnioExist == null)
+                    _context.Attach(detalle.Materia.AnioCarrera);
+                else
+                    detalle.Materia.AnioCarrera = AttachAnioExist;
+
+                _context.Attach(detalle.Materia);
+
+            }
+
+            //busco los detalles existentes para poder encontrar los eliminados
+            var detallesExistentes = _context.detallesinscripciones.Where(d => d.InscripcionId.Equals(inscripcion.Id)).AsNoTracking().ToList();
+            //busco los detalles que se eliminaron
+            var detallesEliminados = detallesExistentes.Where(d => !inscripcion.detallesInscripciones.Any(di => di.Id == d.Id)).ToList();
+            foreach (var detalle in inscripcion.detallesInscripciones)
+            {
+                if (detalle.Id == 0)
+                {
+                    _context.detallesinscripciones.Add(detalle);
+                }
+                else
+                {
+                    _context.Entry(detalle).State = EntityState.Modified;
+                }
+            }
+            foreach (var detalle in detallesEliminados)
+            {
+                _context.detallesinscripciones.Remove(detalle);
+            }
+
             if (id != inscripcion.Id)
             {
                 return BadRequest();
@@ -93,23 +167,70 @@ namespace InstitutoBack.Controllers.Inscripciones
         [HttpPost]
         public async Task<ActionResult<Inscripcion>> PostInscripcion(Inscripcion inscripcion)
         {
-            _context.inscripciones.Add(inscripcion);
-            await _context.SaveChangesAsync();
+            try
+            {
+                if (inscripcion.Carrera != null) _context.Attach(inscripcion.Carrera);
+                if (inscripcion.PeriodoInscripcion != null) _context.Attach(inscripcion.PeriodoInscripcion);
+                if (inscripcion.PeriodoInscripcion.CicloLectivo != null) _context.Attach(inscripcion.PeriodoInscripcion.CicloLectivo);
 
-            return CreatedAtAction("GetInscripcion", new { id = inscripcion.Id }, inscripcion);
+                foreach (var insc_carrera in inscripcion.Alumno.InscripcionesACarreras)
+                {
+                    var AttachCarreraExist = _context.Set<Carrera>().Local.FirstOrDefault(entry => entry.Id.Equals(insc_carrera.Carrera.Id));
+                    if (AttachCarreraExist == null)
+                        _context.Attach(insc_carrera.Carrera);
+                    else
+                        insc_carrera.Carrera = AttachCarreraExist;
+                }
+
+                foreach (var detalle in inscripcion.detallesInscripciones)
+                {
+                    //detalle.Materia.AnioCarrera = null;
+                    var AttachAnioExist = _context.Set<AnioCarrera>().Local.FirstOrDefault(entry => entry.Id.Equals(detalle.Materia.AnioCarrera.Id));
+                    var AttachCarreraExist = _context.Set<Carrera>().Local.FirstOrDefault(entry => entry.Id.Equals(detalle.Materia.AnioCarrera.Carrera.Id));
+                    if (AttachCarreraExist == null)
+                        _context.Attach(detalle.Materia.AnioCarrera.Carrera);
+                    else
+                        detalle.Materia.AnioCarrera.Carrera = AttachCarreraExist;
+                    if (AttachAnioExist == null)
+                        _context.Attach(detalle.Materia.AnioCarrera);
+                    else
+                        detalle.Materia.AnioCarrera = AttachAnioExist;
+
+                    _context.Attach(detalle.Materia);
+
+                }
+                if (inscripcion.Alumno != null) _context.Attach(inscripcion.Alumno);
+
+                _context.inscripcionesExamenes.Add(inscripcion);
+                await _context.SaveChangesAsync();
+            }
+            catch (Exception ex)
+            {
+                throw new InvalidOperationException("Error al adjuntar entidades relacionadas o guardar la inscripción de examen.", ex);
+            }
+
+            return CreatedAtAction("GetInscripcionExamen", new { id = inscripcion.Id }, inscripcion);
         }
 
         // DELETE: api/ApiInscripciones/5
         [HttpDelete("{id}")]
         public async Task<IActionResult> DeleteInscripcion(int id)
         {
-            var inscripcion = await _context.inscripciones.FindAsync(id);
+            var inscripcion = await _context.inscripciones.Include(i => i.Alumno)
+                .Include(i => i.Carrera)
+                .Include(i => i.PeriodoInscripcion)
+                .Include(i => i.detallesInscripciones)
+                    .ThenInclude(d => d.Materia).ThenInclude(m => m.AnioCarreraId)
+                    .FirstOrDefaultAsync(i => i.Id.Equals(id));
             if (inscripcion == null)
             {
                 return NotFound();
             }
-
             inscripcion.Eliminado = true;
+            foreach (var detalle in inscripcion.detallesInscripciones)
+            {
+                _context.detallesinscripciones.Remove(detalle);
+            }
             _context.inscripciones.Update(inscripcion);
             await _context.SaveChangesAsync();
 
