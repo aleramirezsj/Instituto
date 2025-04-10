08fdb2ee62a301497af5bb380ca9edd4576ba824
Rubén Alejandro Ramirez <aleramirezsj@gmail.com>
2025-04-10 00:20:14 -0300

    Mejorar la configuración de la variable `branch`
    Se ha cambiado la asignación de la variable `branch` para que se obtenga del entorno a través de `Environment.GetEnvironmentVariable("BRANCH")`. Si no se encuentra definida, se utiliza "master" como valor predeterminado. Esta modificación proporciona mayor flexibilidad en la configuración del entorno de ejecución.

--- InstitutoServices/Services/Commons/GenericService.cs
+++ InstitutoServices/Services/Commons/GenericService.cs
@@ -29,7 +29,9 @@ namespace InstitutoServices.Services.Commons
         {
             this.client = new HttpClient();
             this.options = new JsonSerializerOptions() { PropertyNameCaseInsensitive = true };
-            string branch = "master";
+            string branch = Environment.GetEnvironmentVariable("BRANCH") ?? "master";
+
+            //string branch = "master";
             string urlApi;
             if (branch == "master")
                 urlApi = "https://api.isp20.edu.ar/api/";
